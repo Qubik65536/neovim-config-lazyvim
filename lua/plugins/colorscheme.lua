b0fc5db5a702f8b764423bcbb0d4da8e79127e5b
Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>
2025-10-28 15:28:11 -0400

    Configure Oxocarbon theme & Python
    - Add Oxocarbon theme.
    - Enable `python` language extra.
    
    Signed-off-by: Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- Add Oxocarbon
+  { "nyoom-engineering/oxocarbon.nvim" },
+
+  -- Set Oxocarbon as colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon",
+    },
+  },
+}
